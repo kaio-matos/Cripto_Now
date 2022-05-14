fa9207e67b0905e61c9ef18c113fa9e5e0e1350f
root <root@DESKTOP-U26BJ62.localdomain>
2022-05-14 11:58:41 -0300

    components/MainHeader: Adding main-header component

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-amber-400">
+    <h1 class="primary-font p-4 text-6xl text-center text-neutral-600">
+      CRIPTO_NOW
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'main-header',
+}
+</script>
