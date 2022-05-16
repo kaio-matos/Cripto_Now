cd8bed6c923bc9263fd271d4eeca1f7552ebf4d9
root <root@DESKTOP-U26BJ62.localdomain>
2022-05-16 09:57:57 -0300

    components/PriceHolder: Creating a price holder component

---
+++ src/components/PriceHolder.vue
@@ -0,0 +1,21 @@
+<template>
+    <span class="text-8xl primary-font text-red-500">
+        {{ currentPrice.toLocaleString(
+                'pt-br',
+                { style: 'currency', currency: 'BRL' }
+            )
+        }}
+    </span>
+</template>
+
+<script>
+export default {
+    name: 'price-holder',
+    props: {
+        currentPrice: {
+            type: Number,
+            required: true
+        }
+    },
+}
+</script>
