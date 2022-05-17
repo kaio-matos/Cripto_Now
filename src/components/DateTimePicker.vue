2b11a01ce93c0db8044135039c0af8f837c989d8
root <root@DESKTOP-U26BJ62.localdomain>
2022-05-17 09:46:13 -0300

    components/DateTimePicker: Creating DateTimePicker based on an external lib picker

---
+++ src/components/DateTimePicker.vue
@@ -0,0 +1,43 @@
+<template>
+    <vue-ctk-date-time-picker :max-date="new Date().toISOString()" label="Select specific date & time" v-model="dateRef"
+        noClearButton no-button-now locale="en">
+    </vue-ctk-date-time-picker>
+</template>
+
+<script>
+
+import { ref, watchEffect } from 'vue';
+import VueCtkDateTimePicker from 'vue-ctk-date-time-picker';
+import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css';
+
+export default {
+    name: 'date-time-picker',
+    components: { VueCtkDateTimePicker },
+    setup(_, { emit }) {
+        const dateRef = ref()
+        watchEffect(() => emit('update:dateTime', dateRef.value))
+
+        return { dateRef }
+    }
+}
+</script>
+
+<style scoped>
+:deep(.header-picker) {
+    background-color: rgb(251 191 36) !important;
+    color: rgb(64 64 64) !important;
+}
+
+/* Override default active effect */
+:deep(.time-picker-column-item.active .time-picker-column-item-effect),
+:deep(.datepicker-container .datepicker-days .datepicker-day.selected .datepicker-day-effect) {
+    background-color: #ff4c29 !important;
+}
+
+/* Override default hover effect */
+:deep(.datepicker-day:hover .datepicker-day-effect),
+:deep(.time-picker-column-item:hover .time-picker-column-item-effect),
+:deep(.custom-button:hover .custom-button-effect) {
+    background-color: #ff4c29 !important;
+}
+</style>
