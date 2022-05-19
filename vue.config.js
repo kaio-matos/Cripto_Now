89f8fc226eb4cbafa22ddfab226da0f8401cec6c
kaio-matos <kaio_matos22@hotmail.com>
2022-05-19 11:08:39 -0300

    vue.config: Configuring publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/Finished-Frontend-Technical-Test/" : "/",
   transpileDependencies: true
 })
